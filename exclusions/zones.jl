d770ecff3083cdf57da028a5707795e573e58a01
sandyspiers <sandy.spiers@curtin.edu.au>
2024-07-26 17:29:37 +0800

    get zones

--- exclusions/zones.jl
+++ exclusions/zones.jl
@@ -1,33 +1,66 @@
 using CoordinatePartitioning: build_edm
+using Plots
+# pythonplot()
+
+using ProgressMeter
 using LinearAlgebra: norm
-using Plots: plot, plot!, contour, contour!, scatter!
 
 rand_loc(n::Integer) = rand(n, 2)
 
-function agg_distance(location, points)
-    return sum(norm(location .- point) for point in points)
+function total_distance(points)
+    return sum(sum_distance(points)(pt) for pt in eachrow(points)) / 2
 end
 
-function threshold(point, points)
-    return location -> agg_distance(location, points) < agg_distance(point, points)
+function sum_distance(points)
+    return loc -> sum(norm(loc .- point) for point in points)
 end
 
-function plot_locs(locations)
-    p = plot(; size=(2000, 2000), xlims=(0, 1), ylims=(0, 1))
-    scatter!(locations[:, 1], locations[:, 2])
-    return p
+function sum_threshold(points::Matrix{T} where {T}, offset=0)
+    sd = sum_distance(points)
+    return loc -> sum(sd(loc) < sd(pt) + offset for pt in eachrow(points))
 end
 
-function plot_cut!(cut)
-    scatter!(cut[:, 1], cut[:, 2]; color=:red)
-    x = y = 0:0.001:1
-    for point in eachrow(cut)
-        contour!(x, y, (x, y) -> threshold(point, cut)([x, y]); levels=1, color=:red)
+function offsets(cuts::Vector{Matrix{T}} where {T})
+    fy = total_distance.(cuts)
+    lb = maximum(fy)
+    p = size(first(cuts))[1]
+    return (lb .- fy) ./ p
+end
+
+function sum_threshold(cuts::Vector{Matrix{T}} where {T}; use_lb=true)
+    if use_lb
+        st = (sum_threshold(cut, offset) for (cut, offset) in zip(cuts, offsets(cuts)))
+    else
+        st = (sum_threshold(cut) for cut in cuts)
+    end
+    max_thresholds(loc) = maximum(s(loc) for s in st)
+    return max_thresholds
+end
+
+function plot_threshold!(cut::Matrix{T} where {T})
+    return plot_threshold!([cut]; use_lb=false, rng=range(0, 1, 300))
+end
+function plot_threshold!(
+    cuts::Vector{Matrix{T}} where {T}; use_lb=false, rng=range(0, 1, 300)
+)
+    st = sum_threshold(cuts; use_lb=use_lb)
+    x = y = rng
+    z = map(((x, y),) -> st([x, y]), Iterators.product(x, y))
+
+    CM = cgrad(:thermal; rev=true)
+    heatmap!(x, y, z; alpha=1, colormap=CM)
+    for cut in cuts
+        contri = invperm(sortperm([sum_distance(cut)(pt) for pt in eachrow(cut)]; rev=true))
+        scatter!(cut[:, 1], cut[:, 2]; zcolor=contri, colormap=CM, markersize=5, alpha=1)
     end
 end
 
-loc = rand_loc(1)
-cuts = (rand_loc(5) for _ in 1:2)
-p = plot_locs(loc)
-plot_cut!.(cuts)
-p
+cuts = [rand_loc(6) for _ in 1:4]
+
+p_fy = plot(; xlims=(0, 1), ylims=(0, 1))
+plot_threshold!(cuts; use_lb=false)
+
+p_lb = plot(; xlims=(0, 1), ylims=(0, 1))
+plot_threshold!(cuts; use_lb=true)
+
+plot(p_fy, p_lb; size=(2000, 1000))

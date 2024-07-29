474ff137671a1c4d48c8d74da57068f0750292fd
sandyspiers <sandy.spiers@curtin.edu.au>
2024-07-29 11:05:47 +0800

    corrected math, some example plots

--- exclusions/zones.jl
+++ exclusions/zones.jl
@@ -1,35 +1,37 @@
 using CoordinatePartitioning: build_edm
 using Plots
-# pythonplot()
+pythonplot()
 
 using ProgressMeter
 using LinearAlgebra: norm
 
 rand_loc(n::Integer) = rand(n, 2)
 
+function sum_distance(points)
+    dist(loc) = sum(norm(loc - pt) for pt in eachrow(points))
+    return dist
+end
+
 function total_distance(points)
     return sum(sum_distance(points)(pt) for pt in eachrow(points)) / 2
 end
 
-function sum_distance(points)
-    return loc -> sum(norm(loc .- point) for point in points)
-end
-
-function sum_threshold(points::Matrix{T} where {T}, offset=0)
-    sd = sum_distance(points)
-    return loc -> sum(sd(loc) < sd(pt) + offset for pt in eachrow(points))
-end
-
-function offsets(cuts::Vector{Matrix{T}} where {T})
+function offsets(cuts::Vector{Matrix{T}} where {T}, lb=0)
     fy = total_distance.(cuts)
-    lb = maximum(fy)
+    lb = max(lb, maximum(fy))
     p = size(first(cuts))[1]
     return (lb .- fy) ./ p
 end
 
-function sum_threshold(cuts::Vector{Matrix{T}} where {T}; use_lb=true)
-    if use_lb
-        st = (sum_threshold(cut, offset) for (cut, offset) in zip(cuts, offsets(cuts)))
+function sum_threshold(points::Matrix{T} where {T}, offset=0)
+    sd = sum_distance(points)
+    thresholds = [sd(pt) + offset for pt in eachrow(points)]
+    return loc -> sum(sd(loc) .< thresholds)
+end
+
+function sum_threshold(cuts::Vector{Matrix{T}} where {T}; use_lb=true, lb=0)
+    if use_lb || lb > 0
+        st = (sum_threshold(cut, offset) for (cut, offset) in zip(cuts, offsets(cuts, lb)))
     else
         st = (sum_threshold(cut) for cut in cuts)
     end
@@ -39,14 +41,24 @@ end
 
 plot_threshold!(cut::Matrix{T} where {T}; kwargs...) = plot_threshold!([cut]; kwargs...)
 function plot_threshold!(
-    cuts::Vector{Matrix{T}} where {T}; use_lb=false, rng=range(0, 1, 500)
+    cuts::Vector{Matrix{T}} where {T}; use_lb=false, lb=0, rng=range(0, 1, 300)
 )
-    st = sum_threshold(cuts; use_lb=use_lb)
+    st = sum_threshold(cuts; use_lb=use_lb, lb=lb)
     x = y = rng
-    z = map(((x, y),) -> st([x, y]), Iterators.product(x, y))
+    z = ((x, y) -> st([x, y])).(x', y)
 
     CM = cgrad(:thermal; rev=true)
-    heatmap!(x, y, z; alpha=1, colormap=CM, colorbar=false)
+    heatmap!(
+        x,
+        y,
+        z;
+        xlims=(0, 1),
+        ylims=(0, 1),
+        aspect_ratio=:equal,
+        alpha=0.6,
+        colormap=CM,
+        colorbar=false,
+    )
     for cut in cuts
         contri = invperm(sortperm([sum_distance(cut)(pt) for pt in eachrow(cut)]; rev=true))
         scatter!(
@@ -56,27 +68,78 @@ function plot_threshold!(
             colormap=CM,
             markersize=5,
             alpha=1,
+            aspect_ratio=:equal,
             colorbar=false,
+            legend=false,
         )
     end
 end
 
-p = 9
-n = 45
-m = 10
-
-locs = rand_loc(n)
-cuts = [rand_loc(p) for _ in 1:m]
-
-single_fy = Plots.Plot[]
-for cut in cuts
-    plt = plot(; xlims=(0, 1), ylims=(0, 1))
+function plot_single_cut(p)
+    cut = rand_loc(p)
+    p = plot(; size=(1000, 1000))
     plot_threshold!(cut; use_lb=false)
-    push!(single_fy, plt)
+    return p
 end
-single_fy = plot(single_fy...; layout=(1, m))
 
-all_lb = plot(; xlims=(0, 1), ylims=(0, 1))
-plot_threshold!(cuts; use_lb=true)
+function plot_multi_cut(p, m)
+    cuts = [rand_loc(p) for _ in 1:m]
+    p = plot(; xlims=(0, 1), ylims=(0, 1))
+    plot_threshold!(cuts; use_lb=true)
+    return p
+end
 
-plot(single_fy, all_lb; size=(1000, 2000), layout=grid(2, 1; heights=[0.3, 0.7]))
+function plot_single_cuts_and_contribution(p, n, m)
+    locs = rand_loc(n)
+    cuts = [rand_loc(p) for _ in 1:m]
+
+    single_fy = Plots.Plot[]
+    for cut in cuts
+        plt = plot()
+        plot_threshold!(cut; use_lb=false)
+        push!(single_fy, plt)
+    end
+    single_fy = plot(single_fy...; layout=(1, m))
+
+    all_lb = plot(; xlims=(0, 1), ylims=(0, 1))
+    plot_threshold!(cuts; use_lb=true)
+    scatter!(locs[:, 1], locs[:, 2]; markersize=5, alpha=1, legend=false)
+
+    return plot(
+        single_fy, all_lb; size=(1000, 1250), layout=grid(2, 1; heights=[0.25, 0.75])
+    )
+end
+
+function plot_single_cuts_expanded_and_contribution(p, n, m)
+    locs = rand_loc(n)
+    cuts = [rand_loc(p) for _ in 1:m]
+
+    single_fy = Plots.Plot[]
+    for cut in cuts
+        plt = plot()
+        plot_threshold!(cut; use_lb=false)
+        push!(single_fy, plt)
+    end
+    single_fy = plot(single_fy...; layout=(1, m))
+
+    single_lb = Plots.Plot[]
+    lb = maximum(total_distance(cut) for cut in cuts)
+    for cut in cuts
+        plt = plot()
+        plot_threshold!(cut; lb=lb)
+        push!(single_lb, plt)
+    end
+    single_lb = plot(single_lb...; layout=(1, m))
+
+    all_lb = plot(; xlims=(0, 1), ylims=(0, 1))
+    plot_threshold!(cuts; use_lb=true)
+    scatter!(locs[:, 1], locs[:, 2]; markersize=5, alpha=1, legend=false)
+
+    return plot(
+        single_fy,
+        single_lb,
+        all_lb;
+        size=(1000, 1500),
+        layout=grid(3, 1; heights=[0.25, 0.25, 0.5]),
+    )
+end

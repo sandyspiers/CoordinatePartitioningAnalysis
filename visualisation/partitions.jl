57c021f7111a028bf4f65a6b803d8684e5174d4c
sandyspiers <sandy.spiers@curtin.edu.au>
2024-08-01 09:11:43 +0800

    better partition plot api

--- visualisation/partitions.jl
+++ visualisation/partitions.jl
@@ -5,22 +5,35 @@ using Plots
 
 include("exclusion_zones.jl")
 
-function get_2d_partitions(edm, strategy)
+function force_2_cols(mat)
+    if ndims(mat) != 2
+        @warn "Matrix has $(ndims(mat)) but should have 2!"
+        return mat
+    end
+    cols = size(mat)[2]
+    if cols > 2
+        return mat[:, 1:2]
+    end
+    if cols == 2
+        return mat
+    end
+    if cols == 1
+        return hcat(mat, zero(mat[:, 1]))
+    end
+    # col=0
+    return reshape([], 0, 2)
+end
+
+function R2_partitions(edm, strategy)
     new_loc, evals = euclid_embed(edm; centered=true)
     num_par = round(Int, ceil(size(new_loc)[2] / 2))
     partitions = partition(evals, num_par, strategy)
-    return [new_loc[:, par] for par in partitions]
+    new_locs = [force_2_cols(new_loc[:, par]) for par in partitions]
+    return new_locs
 end
 
-function scatter_loc(locations, cut=nothing)
-    coords = size(locations)[2]
-    if coords == 0
-        return scatter()
-    end
-    if coords == 1
-        locations = hcat(locations, zero(locations[:, 1]))
-    end
-    s = scatter(locations[:, 1], locations[:, 2])
+function plot_partition(locations, cut=nothing)
+    s = scatter(eachcol(force_2_cols(locations))...)
     if !isnothing(cut)
         plot_threshold!(locations[cut, :]; squared=true, other_locs=locations)
     end
@@ -30,7 +43,7 @@ end
 function plot_partitions(locations, strategy="random", cut=nothing)
     edm = build_edm(locations)
     return plot(
-        [scatter_loc(loc, cut) for loc in get_2d_partitions(edm, strategy)]...;
+        (plot_partition(loc, cut) for loc in R2_partitions(edm, strategy))...;
         link=:all,
         legend=false,
         ticks=false,
